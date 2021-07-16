0f5699be70aca1b7aacd1dccb4b33a444d6748cb
Aleksey Kuflievskiy <kuflievskiy@gmail.com>
2021-07-16 22:35:08 +0300

    useful ubuntu directories
    useful ubuntu directories

---
+++ devops/ubuntu/directories.bash
@@ -0,0 +1,14 @@
+# for current user only
+~/.local/bin
+
+# for all
+/usr/local/bin
+
+# scheduled crontabs
+/var/spool/cron/crontabs
+
+/var/run 
+# The location of the pid file should be configurable. /var/run is standard for pid files,
+
+/var/log
+# The same as /var/log is standard for logs.
