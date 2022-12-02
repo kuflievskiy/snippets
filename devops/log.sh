1ea5a77fdda7a6d104f1b1c8ac10d8bf2c898b03
Aleksey Kuflievskiy <kuflievskiy@gmail.com>
2022-12-02 20:28:57 +0200

    Update log.sh

--- devops/log.sh
+++ devops/log.sh
@@ -3,3 +3,10 @@ grep -e 'report.ERROR:' -e '\[2022' | \
 awk -F 'report.ERROR: ' '{print $2}' | \
 sort | uniq -c | sort -rn | \
 sed '/^$/d' > system-summary.log
+
+
+
+sudo grep -rnw 'Fatal' /var/log | \
+awk -F 'Fatal error: ' '{print $2}' |  \
+sort | uniq -c | sort -rn | \
+tail -n 10
